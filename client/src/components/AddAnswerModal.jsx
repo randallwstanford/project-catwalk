340ade4a036ca8fdee7965889b0519da8330b315
randallwstanford <randallwstanford@gmail.com>
2021-09-13 12:07:59 -0600

    added modal component

---
+++ client/src/components/AddAnswerModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddAnswerModal = () => {
+  return (
+    <div className="modal">
+      <div className="test">
+        you will be a modal soon
+      </div>
+    </div>
+  );
+};
+
+export default AddAnswerModal;
