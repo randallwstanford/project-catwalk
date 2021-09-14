fff65cc953d80a486a44d6758a4e5713484ed17f
randallwstanford <randallwstanford@gmail.com>
2021-09-14 11:19:47 -0600

    removed unecessary modal tag inside app.jsx.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import QandA from './QandA.jsx';
-import AddAnswerModal from './AddAnswerModal.jsx';
 import RatingsReviews from './Ratings&Reviews/RatingsReviews.jsx';
 
 export default class App extends React.Component {
@@ -12,7 +11,6 @@ export default class App extends React.Component {
     return (
       <div>
         <QandA />
-        <AddAnswerModal />
         <RatingsReviews />
       </div>
     );
