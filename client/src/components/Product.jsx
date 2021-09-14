6dbf48cfa40cf4596c3ad8d29fdf6826ec15cb02
Shirin <shirinetezazian@gmail.com>
2021-09-14 10:45:51 -0600

    Rename branch to product-overview and implement initial component setup

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -7,15 +7,54 @@ export default function Product() {
     <div>
       <div className="flex-container">
         <div id="image-gallery">images</div>
-        <div id="product-information">details
-          <div>
-            <h3>CATEGORY</h3>
+        <div id="product-information">
+          <div className="review-flex-container">
+            <span id="star-checked">★</span>
+            <span id="star-checked">★</span>
+            <span id="star-checked">★</span>
+            <span id="star">★</span>
+            <span id="star">★</span>
+            <span>
+              <h6>Read all reviews</h6>
+            </span>
           </div>
           <div>
-            <h1>Expanded Product Name</h1>
+            <h4>CATEGORY</h4>
           </div>
-          <button>SELECT SIZE</button>
-          <button>ADD TO BAG</button>
+          <div>
+            <h1>Product Name</h1>
+          </div>
+          <div className="style-selector">
+            <input
+              type="radio"
+            />
+            <input
+              type="radio"
+            />
+            <input
+              type="radio"
+            />
+            <input
+              type="radio"
+            />
+          </div>
+          <div className="price">$</div>
+          <div className="size-quantity">
+            <input
+              name="size"
+              type="number"
+              placeholder="Select Size"
+            />
+            <input
+              name="quantity"
+              type="number"
+              placeholder="1"
+            />
+          </div>
+          <div className="buttons">
+            <button>Add to bag</button>
+            <button>★</button>
+          </div>
         </div>
       </div>
       <div id="product-overview">
