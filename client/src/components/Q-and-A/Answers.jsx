d14488244fb304165d603e626ec4450afbc74503
randallwstanford <randallwstanford@gmail.com>
2021-09-15 10:08:27 -0600

    added Answer text component

---
+++ client/src/components/Q-and-A/Answers.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Answers = (props) => {
+  const checkForAnswer = (answer) => {
+    if (answer.props === undefined) { return 'Not answered yet'; }
+    return answer.props.body;
+  };
+
+  const checkForSeller = (answer) => {
+    if (answer.props === undefined) { return 'ddd'; }
+    if (answer.props !== 'Seller') { return answer.props.answerer_name; }
+    return (<b>Seller</b>);
+  };
+
+  const checkForHelpfulness = (answer) => {
+    if (answer === undefined) { return 0; }
+    return answer.helpfulness;
+  };
+
+  return (
+    <div>
+      <div>{checkForAnswer(props)}</div>
+      By: {checkForSeller(props)}
+    </div>
+  );
+};
+
+export default Answers;
