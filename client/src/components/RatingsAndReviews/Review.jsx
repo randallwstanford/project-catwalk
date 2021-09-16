c092ab8e50146b94f63240de4348ebffb174cf06
Jason-K-87 <jason.kenan.west@gmail.com>
2021-09-16 10:51:47 -0600

    code cleanup and switched to api data`

--- client/src/components/RatingsAndReviews/Review.jsx
+++ client/src/components/RatingsAndReviews/Review.jsx
@@ -1,19 +1,3 @@
-// import React from 'react';
-
-// const Review = () => (
-//   <div id="ReviewContainer">
-//     <div id="starRating">StarRating</div>
-//     <div id="verifiedPurchaser">Verified Purchaser</div>
-//     <div id="reviewUserName">userName</div>
-//     <div id="reviewDate">Date</div>
-//     <div id="reviewTitle">Title</div>
-//     <div id="reviewBody">Body</div>
-//     <div id="reviewHelpful">Helpful</div>
-//     <div id="reviewReport">Report</div>
-//   </div>
-// );
-// export default Review;
-
 import React from 'react';
 
 const Review = (props) => {
