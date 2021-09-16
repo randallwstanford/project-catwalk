61081c073d1923ab93a2e8b32eaf12cd3a94af2c
randallwstanford <randallwstanford@gmail.com>
2021-09-16 11:47:28 -0600

    minor change

--- client/src/components/Q-and-A/QandA.jsx
+++ client/src/components/Q-and-A/QandA.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable no-else-return */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable prefer-template */
 /* eslint-disable max-len */
 /* eslint-disable array-callback-return */
 
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import AddQuestionModal from './AddQuestionModal.jsx';
 import AddAnswerModal from './AddAnswerModal.jsx';
@@ -15,13 +14,6 @@ import * as utils from './utils/QandA.utils.js';
 
 const QandA = () => {
   const { product } = useContext(appContext);
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    axios.get(`/qa/questions/?product_id=${product.id}`)
-      .then((res) => { setQuestions((res.data.results).sort((a, b) => b.question_helpfulness - a.question_helpfulness)); })
-      .catch((err) => console.log(err));
-  }, []);
 
   // Hides 'More Answered Questions' Button if less than 2 questions
   const checkForAnsweredQuestions = () => {
@@ -32,18 +24,6 @@ const QandA = () => {
           : console.log('there are more than 2 questions');
       })
       .catch((err) => console.log(err));
-  };
-
-  // fitlers but doesnt rerender
-  const handleSearch = (event) => {
-    var term = event.target.value;
-    if (term.length >= 3) {
-      setQuestions(questions.filter((question) => {
-        question.question_body.toLowerCase().includes(term) === true
-          ? question
-          : 'No Results Found';
-      }));
-    }
   };
 
   return (
@@ -57,7 +37,6 @@ const QandA = () => {
             className="question-input"
             type="text"
             placeholder="Have a question? Search for answers."
-            onChange={handleSearch}
           />
         </form>
       </div>
