396bbf769553d19cd7141c03afd40277760ce662
randallwstanford <randallwstanford@gmail.com>
2021-09-17 18:41:16 -0600

    load more answer button now workin hard!

--- client/src/components/Q-and-A/AllQandA.jsx
+++ client/src/components/Q-and-A/AllQandA.jsx
@@ -5,25 +5,7 @@ import * as utils from './utils/AllQandA.utils.js';
 import { appContext } from '../../contexts/index.js';
 import Answers from './Answers.jsx';
 
-<<<<<<< HEAD
-const AllQandA = () => {
-  const { product } = useContext(appContext);
-  const [questions, setQuestion] = useState([]);
-
-  useEffect(() => {
-    axios.get(`/qa/questions/?product_id=${product.id}`)
-      .then((res) => {
-        // sort greatest to least in helpfulness
-        setQuestion((res.data.results).sort((a, b) => {
-          return b.question_helpfulness - a.question_helpfulness;
-        }));
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-=======
 const AllQandA = (props) => {
->>>>>>> a1589d2ab8a735b44c3f8754c79b68599dbe2007
   return (
     props.questions.map((question, index) => {
       return (
@@ -41,24 +23,14 @@ const AllQandA = (props) => {
             <div className={`answer${index} answer`}><b>A:</b>
               {/* ------------- Answers Text ------------- */}
               <Answers answers={question.answers} />
-<<<<<<< HEAD
             </div>
             {/* ------------- Date -------------*/}
             <div>&nbsp;<b>Date:</b> {utils.formatDate(question.question_date)}</div>
             <div>&nbsp;Helpful?
               <button className="yes-button" onClick={() => console.log('incremenet dis')}> Yes </button>
-              ( {utils.checkForHelpfulness(question.answers[Object.keys(question.answers)[0]])} ) | Report
+              ( {utils.checkForHelpfulness(question.answers[Object.keys(question.answers)[0]])} )
+              | Report
             </div>
-=======
-            </span>
-          </div>
-          {/* ------------- Date -------------*/}
-          <div>&nbsp;<b>Date:</b> {utils.formatDate(question.question_date)}</div>
-          <div>&nbsp;Helpful?
-            <button className="yes-button" onClick={() => console.log('incremenet dis')}> Yes </button>
-            ( {utils.checkForHelpfulness(question.answers[Object.keys(question.answers)[0]])} )
-            | Report
->>>>>>> a1589d2ab8a735b44c3f8754c79b68599dbe2007
           </div>
         </div>
       );
--- client/src/components/Q-and-A/utils/Answers.utils.js
+++ client/src/components/Q-and-A/utils/Answers.utils.js
@@ -3,11 +3,11 @@ export const checkForAnswer = (answer) => {
   return answer.body;
 };
 
-export const checkForSeller = (answer) => {
-  if (answer === undefined) { return 'ddd'; }
-  if (answer !== 'Seller') { return answer.answerer_name; }
-  return 'Seller';
-};
+// export const checkForSeller = (answer) => {
+//   if (answer === undefined) { return 'ddd'; }
+//   if (answer !== 'Seller') { return answer.answerer_name; }
+//   return 'Seller';
+// };
 
 export const checkForHelpfulness = (answer) => {
   if (answer === undefined) { return 0; }
