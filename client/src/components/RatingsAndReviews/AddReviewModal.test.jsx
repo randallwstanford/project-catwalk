3c7eb370e6b1bd75ec42761bdabc62cea7468a59
Jason-K-87 <jason.kenan.west@gmail.com>
2021-09-22 12:10:04 -0600

    Created dummy test Json

--- client/src/components/RatingsAndReviews/AddReviewModal.test.jsx
+++ client/src/components/RatingsAndReviews/AddReviewModal.test.jsx
@@ -4,34 +4,7 @@ import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
 import AddReviewModal from './AddReviewModal.jsx';
 import { reviewsContext } from '../../contexts/index.js';
-
-
-const reviews = [
-  {
-    "review_id": 720891,
-    "rating": 5,
-    "summary": "This product was great!",
-    "recommend": true,
-    "response": "",
-    "body": "I really did or did not like this product based on whether it was sustainably sourced. Then I found out that its made from nothing at all.",
-    "date": "2019-01-01T00:00:00.000Z",
-    "reviewer_name": "funtime",
-    "helpfulness": 8,
-    "photos": []
-  },
-  {
-    "review_id": 720892,
-    "rating": 4,
-    "summary": "This product was ok!",
-    "recommend": false,
-    "response": "",
-    "body": "I really did not like this product solely because I am tiny and do not fit into it.",
-    "date": "2019-01-11T00:00:00.000Z",
-    "reviewer_name": "mymainstreammother",
-    "helpfulness": 2,
-    "photos": []
-  }
-];
+import { reviews } from './dummyTestData.json';
 
 test('to be defined"', () => {
   const component = renderer.create(
