2995f5d6dff2985c93e5763bc3b5249eda5e17c7
Jason-K-87 <jason.kenan.west@gmail.com>
2021-09-25 14:58:12 -0600

    implemented styling for review modal using grid

--- client/src/components/RatingsAndReviews/AddReviewModal.jsx
+++ client/src/components/RatingsAndReviews/AddReviewModal.jsx
@@ -34,11 +34,11 @@ const AddReviewModal = () => {
 
   return (
     <div className="reviewModal" id="addReviewModal" style={{ 'visibility': modalVisibility }}>
-      <button id="closeModal" className="closeModal" onClick={() => { toggleModal(modalVisibility, setModalVisibility); }}>X</button>
+      <button id="closeReviewModal" className="closeReviewModal" onClick={() => { toggleModal(modalVisibility, setModalVisibility); }}>X</button>
       <h2 id="addReviewTitle">Write your review</h2>
       <h3 id="addReviewSubtitle">About the {product.name}</h3>
-      <span className="notice">fields marked with * are required</span>
-      <form className="addReview" onSubmit={handleSubmit} onChange={handleChange}>
+      <span id="requiredNotice" className="notice">fields marked with * are required</span>
+      <form id="addReview" className="addReview" onSubmit={handleSubmit} onChange={handleChange}>
         <div id="overallRating">
           <label> * Overall rating:</label>
           <input onChange={() => { setOverallRating(1); }} type="radio" value="1" checked={overallRating === 1}></input>
@@ -56,23 +56,35 @@ const AddReviewModal = () => {
           <input onChange={() => { setRecommend(false); }} type="radio" value="no" checked={recommend === false}></input>
         </div>
         <div id="characteristics">
-          <CharacteristicRadioButtons rating={sizeRating} setRating={setSizeRating} labelName="Size" />
-          <div className="radioMeaning"><span className="leftMeaning">Too small</span> <span className="rightmeaning">Too large</span></div>
+          <div id="sizeCharacteristic">
+            <CharacteristicRadioButtons rating={sizeRating} setRating={setSizeRating} labelName="Size" />
+          </div>
+          <div id="sizeRadioMeaning" className="radioMeaning"><span className="leftMeaning">Too small</span> <span className="rightMeaning">Too large</span></div>
 
-          <CharacteristicRadioButtons rating={widthRating} setRating={setWidthRating} labelName="Width" />
-          <div className="radioMeaning"><span className="leftMeaning">Too narrow</span> <span className="rightmeaning">Too wide</span></div>
+          <div id="widthCharacteristic">
+            <CharacteristicRadioButtons rating={widthRating} setRating={setWidthRating} labelName="Width" />
+          </div>
+          <div id="widthRadioMeaning" className="radioMeaning"><span className="leftMeaning">Too narrow</span> <span className="rightMeaning">Too wide</span></div>
 
-          <CharacteristicRadioButtons rating={lengthRating} setRating={setLengthRating} labelName="Length" />
-          <div className="radioMeaning"><span className="leftMeaning">Too short</span> <span className="rightmeaning">Too long</span></div>
+          <div id="lengthCharacteristic">
+            <CharacteristicRadioButtons rating={lengthRating} setRating={setLengthRating} labelName="Length" />
+          </div>
+          <div id="lengthRadioMeaning" className="radioMeaning"><span className="leftMeaning">Too short</span> <span className="rightMeaning">Too long</span></div>
 
-          <CharacteristicRadioButtons rating={fitRating} setRating={setFitRating} labelName="Fit" />
-          <div className="radioMeaning"><span className="leftMeaning">Too tight</span> <span className="rightmeaning">Too loose</span></div>
+          <div id="fitCharacteristic">
+            <CharacteristicRadioButtons rating={fitRating} setRating={setFitRating} labelName="Fit" />
+          </div>
+          <div id="fitRadioMeaning" className="radioMeaning"><span className="leftMeaning">Too tight</span> <span className="rightMeaning">Too loose</span></div>
 
-          <CharacteristicRadioButtons rating={qualityRating} setRating={setQualityRating} labelName="Quality" />
-          <div className="radioMeaning"><span className="leftMeaning">Poor</span> <span className="rightmeaning">Perfect</span></div>
+          <div id="qualityCharacteristic">
+            <CharacteristicRadioButtons rating={qualityRating} setRating={setQualityRating} labelName="Quality" />
+          </div>
+          <div id="qualityRadioMeaning" className="radioMeaning"><span className="leftMeaning">Poor</span> <span className="rightMeaning">Perfect</span></div>
 
-          <CharacteristicRadioButtons rating={comfortRating} setRating={setComfortRating} labelName="Comfort" />
-          <div className="radioMeaning"><span className="leftMeaning">Uncomfortable</span> <span className="rightmeaning">Perfect</span></div>
+          <div id="comfortCharacteristic">
+            <CharacteristicRadioButtons rating={comfortRating} setRating={setComfortRating} labelName="Comfort" />
+          </div>
+          <div id="comfortRadioMeaning" className="radioMeaning"><span className="leftMeaning">Uncomfortable</span> <span className="rightMeaning">Perfect</span></div>
         </div>
         <label id="reviewSummaryLabel" htmlFor="reviewSummary">Summary:
           <textarea
@@ -80,7 +92,7 @@ const AddReviewModal = () => {
             min="1"
             max="60"
             rows={3}
-            cols={50}
+            cols={45}
             name="reviewSummary"
             id="submitReviewSummary"
             onChange={() => { setSummary(document.getElementById('submitReviewSummary').value); }}
@@ -95,7 +107,7 @@ const AddReviewModal = () => {
             maxLength="1000"
             name="reviewBody"
             rows={5}
-            cols={50}
+            cols={45}
             id="submitReviewBody"
             onChange={() => { setReviewBody(document.getElementById('submitReviewBody').value); }}
             value={reviewBody}
